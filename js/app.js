50d0cbe2b0901ffeffb486c04a449b8a7dffc6fe
AndreyVT <TupeykoAV@gmail.com>
2015-01-25 11:26:23 +0700

    test routing app for angularjs is done!

--- js/app.js
+++ js/app.js
@@ -6,10 +6,32 @@
  * To change this template use File | Settings | File Templates.
  */
 angular.module('enterprise', ['ngRoute']).
-    config(function($routeProvider) {
+    config(function($routeProvider, $locationProvider) {
+        //$locationProvider.html5Mode(true);
         $routeProvider.
-            when("/", {templateUrl:"partials/list.html"})
+            when("/", {templateUrl:"partials/list.html"}).
+            when("/new", {templateUrl:"partials/edit.html", controller:"NewCtrl"}).
+            when("/edit/:id", {templateUrl:"partials/edit.html", controller:"EditCtrl"}).
+            otherwise({redirectTo:"/"});
     })
+
+function EditCtrl($scope, $location, $routeParams)    {
+    $scope.person = $scope.crew[$routeParams.id];
+
+    $scope.save = function(){
+        $location.path("/");
+    }
+}
+
+function NewCtrl($scope, $location)    {
+    $scope.person = {name:"", description:""};
+
+    $scope.save = function(){
+        $scope.crew.push($scope.person);
+        $location.path("/");
+    }
+
+}
 
 function AppCtrl($scope)    {
     $scope.crew =   [
